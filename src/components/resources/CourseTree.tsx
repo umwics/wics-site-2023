776351ebf17e042d4d1d95aaaa2a30f0c41fdf89
Juhee Kim <julieK1217@gmail.com>
2020-10-25 10:18:11 -0500

    errorfix

--- src/components/resources/CourseTree.tsx
+++ src/components/resources/CourseTree.tsx
@@ -1,28 +1,6 @@
-import { makeStyles, Theme } from "@material-ui/core/styles";
 import React from "react";
 
-const useTreeStyles = makeStyles((_theme: Theme) => ({
-    outline: {
-        textAlign: "center",
-        backgroundColor: "#00bfa5",
-        borderRadius: 2,
-        height: 4,
-        width: 40,
-        marginBottom: 25
-    },
-    centered: {
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center"
-    },
-    heroButtons: {
-        marginTop: _theme.spacing(4)
-    }
-}));
-
 const CourseTree: React.FC = () => {
-    const classes = useTreeStyles();
-
     return (
         <div id="tree">
             <iframe
@@ -33,7 +11,6 @@ const CourseTree: React.FC = () => {
                 id="test"
                 frameBorder="0"
                 scrolling="yes"
-                align="left"
             ></iframe>
         </div>
     );
