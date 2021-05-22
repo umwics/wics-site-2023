6d61d7332a6b562ba86bbb6dbb47270beef434ce
Andrea Abellera <58119036+andreaabellera@users.noreply.github.com>
2021-05-22 17:15:05 -0500

    OutreachRecycler Html structure added

--- src/components/OutreachRecycler.tsx
+++ src/components/OutreachRecycler.tsx
@@ -1,11 +1,60 @@
 import { createStyles, makeStyles } from "@material-ui/core/styles";
 import React from "react";
-import Carousel from "react-material-ui-carousel";
 
 const useStyles = makeStyles(() =>
     createStyles({
         root: {
             flexGrow: 1
+        },
+        componentContainer:{
+            height: "30vw",
+            width: "100%",
+            backgroundColor: "beige",
+            display: "flex",
+            justifyContent: "center"
+        },
+        recyclerContainer:{
+            height: "100%",
+            width: "75%",
+            backgroundColor: "black",
+            display: "flex",
+            flexDirection: "column"
+        },
+        recycler:{
+            height: "40%",
+            margin: "1em",
+            backgroundColor: "crimson",
+            display: "grid",
+            gridTemplateColumns: "5em 1fr 5em"
+        },
+        arrowButton:{
+            backgroundColor: "orchid"
+        },
+        recyclerSlide:{
+            backgroundColor: "olive",
+            margin: "0em 5em 0em 5em",
+            display: "grid",
+            gridTemplateColumns: "33% 1fr 33%"
+        },
+        recyclerImgCenter:{
+            backgroundColor: "lightgrey",
+            margin: "1.5em",
+            borderColor: "darkorange",
+            borderStyle: "groove",
+            borderWidth: "thick"
+        },
+        recyclerImgSide:{
+            backgroundColor: "grey",
+            margin: "3em"
+        },
+        descriptionContainer:{
+            backgroundColor: "azure",
+            margin: "0em 4em 1em 4em",
+            overflowY: "scroll"
+        },
+        description:{
+            margin: "3em",
+            color: "navy"
         },
         slideshow: {
             textAlign: "center",
@@ -74,16 +123,34 @@ const OutreachRecycler: React.FC = () => {
     const classes = useStyles();
 
     return (
-        <Carousel {...CarouselSetting}>
-            {items.map(item => {
-                return (
-                    <div key={item.name} className={classes.slideshow}>
-                        <img src={item.src} alt={item.name} />
-                        <div className={classes.boxFilter}></div>
+        // <Carousel {...CarouselSetting}>
+        //     {items.map(item => {
+        //         return (
+        //             <div key={item.name} className={classes.slideshow}>
+        //                 <img src={item.src} alt={item.name} />
+        //                 <div className={classes.boxFilter}></div>
+        //             </div>
+        //         );
+        //     })}
+        // </Carousel>
+        <div className={classes.componentContainer}>
+            <div className={classes.recyclerContainer}>
+                <div className={classes.recycler}>
+                    <div className={classes.arrowButton}></div>
+                    <div className={classes.recyclerSlide}>
+                        <div className={classes.recyclerImgSide}></div>
+                        <div className={classes.recyclerImgCenter}></div>
+                        <div className={classes.recyclerImgSide}></div>
                     </div>
-                );
-            })}
-        </Carousel>
+                    <div className={classes.arrowButton}></div>
+                </div>
+                <div className={classes.descriptionContainer}>
+                    <div className={classes.description}>
+                        The meteor is heading our way, to the balcony. There was panicking and some scurrying around as we retreat inside. Sentinels find their ground, reluctantly ready to face this odd, ill-scheduled calamity. I gape in awe as the meteor takes form of a maiden, and the fire becomes a fiery dress billowing. In her hand is a huge sword crafted like the Phoenix. I stand with the others, mesmerized, watching the remains of her dark cape disintegrate into prismatic ash. Staffina has arrived. And now, she owns the Nyx Ethereal. I have never felt so defeated.
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 };
 
